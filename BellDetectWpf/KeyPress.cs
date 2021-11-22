6470ffeb4e9020debc976b5950c3acb353569e05
Tim Barnes <tjbarnes23@gmail.com>
2021-11-22 08:42:25 -0500

    Add project files.

---
+++ BellDetectWpf/KeyPress.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BellDetectWpf
+{
+    public static class KeyPress
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint SendInput(uint nInputs, Input[] pInputs, int cbSize);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetMessageExtraInfo();
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MouseInput
+        {
+            public int dx;
+            public int dy;
+            public uint mouseData;
+            public uint dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct KeyboardInput
+        {
+            public ushort wVk;
+            public ushort wScan;
+            public uint dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct HardwareInput
+        {
+            public uint uMsg;
+            public ushort wParamL;
+            public ushort wParamH;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct InputUnion
+        {
+            [FieldOffset(0)] public MouseInput mi;
+            [FieldOffset(0)] public KeyboardInput ki;
+            [FieldOffset(0)] public HardwareInput hi;
+        }
+
+        public struct Input
+        {
+            public int type;
+            public InputUnion u;
+        }
+
+        [Flags]
+        public enum KeyEventF
+        {
+            KeyDown = 0x0000,
+            KeyUp = 0x0002,
+            Scancode = 0x0008
+        }    
+
+        public static void Press()
+        {
+            Input[] inputs = new Input[]
+            {
+                new Input
+                {
+                    type = 1, // Keyboard,
+                    u = new InputUnion
+                    {
+                        ki = new KeyboardInput
+                        {
+                            wVk = 0,
+                            wScan = 0x11, // W
+                            dwFlags = (uint)(KeyEventF.KeyDown | KeyEventF.Scancode),
+                            dwExtraInfo = GetMessageExtraInfo()
+                        }
+                    }
+                },
+                new Input
+                {
+                    type = 1, // Keyboard
+                    u = new InputUnion
+                    {
+                        ki = new KeyboardInput
+                        {
+                            wVk = 0,
+                            wScan = 0x11, // W
+                            dwFlags = (uint)(KeyEventF.KeyUp | KeyEventF.Scancode),
+                            dwExtraInfo = GetMessageExtraInfo()
+                        }
+                    }
+                }
+            };
+
+            _ = SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(Input)));
+        }
+    }
+}
---
+++ BellDetectWpf/ViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BellDetectWpf
+{
+    public static class ViewModel
+    {
+        private static string startStopTxt;
+        public static event EventHandler StartStopTxtChanged;
+        
+        public static MainWindow Mw { get; set; }
+
+        public static string StartStopTxt
+        {
+            get
+            {
+                return startStopTxt;
+            }
+
+            set
+            {
+                if (startStopTxt != value)
+                {
+                    startStopTxt = value;
+
+                    // The loading of a method to call into the StartStopTxtChanged event is done by the compiler,
+                    // which sees we have a XAML element that we are binding to a property called StartStopTxt,
+                    // and it checks if there is a corresponding StartStopTxtChanged event.
+                    // If there is, it loads a built-in method to be called into the event, and two-way binding is therefore established.
+                    StartStopTxtChanged?.Invoke(null, EventArgs.Empty);
+                }
+            }
+        }
+
+        public static void Initialize()
+        {
+            Mw = new MainWindow();
+            StartStopTxt = "Start detecting";
+            Mw.Activate();
+        }
+
+        public static async Task StartStop()
+        {
+            if (StartStopTxt == "Start detecting")
+            {
+                StartStopTxt = "Stop detecting";
+                await StartDetecting();
+            }
+            else
+            {
+                StartStopTxt = "Start detecting";
+            }
+        }
+
+        public static async Task StartDetecting()
+        {
+            await Task.Delay(2000);
+
+            do
+            {
+                KeyPress.Press();
+
+                await Task.Delay(1000);
+            }
+            while (StartStopTxt == "Stop detecting");
+        }
+    }
+}
