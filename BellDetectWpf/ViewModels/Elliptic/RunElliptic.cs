931d63ee9a1dedca4cb0fd9ccb1e76438617d417
Tim Barnes <tjbarnes23@gmail.com>
2022-02-04 20:24:04 -0500

    Elliptic filter now calculated using second order sections (SOS)

--- BellDetectWpf/ViewModels/Elliptic/RunElliptic.cs
+++ BellDetectWpf/ViewModels/Elliptic/RunElliptic.cs
@@ -12,6 +12,134 @@ namespace BellDetectWpf.ViewModels.Elliptic
     {
         public static async Task RunElliptic()
         {
+            double[,] num = new double[9,3]
+            {
+                {
+                    0.5728834589769, 0, 0
+                },
+                {
+                    1, -1.962623639221, 1
+                },
+                {
+                    0.5728834589769, 0, 0
+                },
+                {
+                    1, -1.973531393315, 1
+                },
+                {
+                    0.1743216967309, 0, 0
+                },
+                {
+                    1, -1.965861570607, 1
+                },
+                {
+                    0.1743216967309, 0, 0
+                },
+                {
+                    1, -1.971015605825, 1
+                },
+                {
+                    1, 0, 0
+                }
+            };
+        
+            double[,] denom = new double[9,3]
+            {
+                {
+                    1, 0, 0
+                },
+                {
+                    1, -1.965355698953, 0.9976393984693
+                },
+                {
+                    1, 0, 0
+                },
+                {
+                    1, -1.967122341459, 0.9977020125669
+                },
+                {
+                    1, 0, 0 
+                },
+                {
+                    1, -1.966125958653, 0.9993731884407
+                },
+                {
+                    1, 0, 0
+                },
+                {
+                    1, -1.969662838394, 0.9994068636127
+                },
+                {
+                    1, 0, 0
+                }
+            };
+
+            EllipticVM.FilteredWaveformArrDbl = new double[WaveformVM.NumSamples];
+
+            double[,] y;
+            double nu;
+            double de;
+            int o;
+
+            await C_Shared.Status("Applying elliptic filter...", "black", 10, false);
+
+            y = new double[WaveformVM.NumSamples, 9];
+
+            // Loop through audio samples
+            for (int i = 0; i < WaveformVM.NumSamples; i++)
+            {
+                // Loop through rows of coefficients
+                for (int j = 0; j < num.GetLength(0); j++)
+                {
+                    nu = 0.0;
+                    de = 0.0;
+
+                    // Loop through columns of numerator coefficients
+                    for (int k = 0; k < num.GetLength(1); k++)
+                    {
+                        o = i - k;
+
+                        if (o >= 0)
+                        { 
+                            if (j == 0)
+                            {
+                                nu += num[j, k] * WaveformVM.WaveformArr[o];
+                            }
+                            else
+                            {
+                                nu += num[j, k] * y[o, j - 1];
+                            }
+                        }
+                    }
+
+                    // Loop through columns of denominator coefficients
+                    for (int k = 1; k < num.GetLength(1); k++)
+                    {
+                        o = i - k;
+
+                        if (o >= 0)
+                        {
+                            de += denom[j, k] * y[o, j];
+                        }
+                    }
+
+                    y[i, j] = nu - de;
+                }
+            }
+
+            // Convert to short (16-bit) and overwrite original values in .wav file
+            for (int i = 0; i < WaveformVM.NumSamples; i++)
+            {
+                WaveformVM.WaveformArr[i] = (short)Math.Round(y[i, 8] * 3);
+            }
+
+            EllipticVM.FilePathName = String.Empty;
+
+            await C_Shared.Status("Elliptic filter applied", "black", 3000, true);
+        }
+
+        public static async Task RunEllipticFromTF()
+        {
             double[] num = new double[]
             {
                 0.009973221271135,
@@ -84,7 +212,7 @@ namespace BellDetectWpf.ViewModels.Elliptic
                         yy = EllipticVM.FilteredWaveformArrDbl[k];
                     }
 
-                    y2 +=  denom[j] * yy;
+                    y2 += denom[j] * yy;
                 }
 
                 EllipticVM.FilteredWaveformArrDbl[i] = y1 - y2;
