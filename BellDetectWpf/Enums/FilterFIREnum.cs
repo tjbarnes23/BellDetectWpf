721ed33f75e1e4be0599c8d992d652601d0c981c
Tim Barnes <tjbarnes23@gmail.com>
2022-06-02 20:05:14 -0400

    Add 4 new FIR filters with 661 order

--- BellDetectWpf/Enums/FilterFIREnum.cs
+++ BellDetectWpf/Enums/FilterFIREnum.cs
@@ -12,12 +12,16 @@ namespace BellDetectWpf.Enums
     {
         pm_C8_1,
         pm_C8_2,
+        pm_C8_3,
         pm_B9_1,
         pm_B9_2,
+        pm_B9_3,
         pm_D14_1,
         pm_D14_2,
+        pm_D14_3,
         pm_C15_1,
-        pm_C15_2
+        pm_C15_2,
+        pm_C15_3
     }
 
     public static class FilterFIRExt
@@ -26,12 +30,16 @@ namespace BellDetectWpf.Enums
         {
             {FilterFIREnum.pm_C8_1, "PM C8; 923/1040/1060/1177 Hz; -30/-5/-30 dB; 276 order; 48 kHz"},
             {FilterFIREnum.pm_C8_2, "PM C8; 1002/1040/1060/1189 Hz; -40/-3/-40 dB; 1366 order; 48 kHz"},
+            {FilterFIREnum.pm_C8_3, "PM C8; 923/1040/1060/1177 Hz; -60/-3/-60 dB; 661 order; 48 kHz"},
             {FilterFIREnum.pm_B9_1, "PM B9; 865/982/1002/1119 Hz; -30/-5/-30 dB; 276 order; 48 kHz"},
             {FilterFIREnum.pm_B9_2, "PM B9; 891/982/1002/1040 Hz; -40/-3/-40 dB; 1366 order; 48 kHz"},
+            {FilterFIREnum.pm_B9_3, "PM B9; 865/982/1002/1119 Hz; -60/-3/-60 dB; 661 order; 48 kHz"},
             {FilterFIREnum.pm_D14_1, "PM D14; 464/581/601/718 Hz; -30/-5/-30 dB; 276 order; 48 kHz"},
             {FilterFIREnum.pm_D14_2, "PM D14; 532/581/601/647 Hz; -40/-3/-40 dB; 1129 order; 48 kHz"},
+            {FilterFIREnum.pm_D14_3, "PM D14; 464/581/601/718 Hz; -60/-5/-60 dB; 661 order; 48 kHz"},
             {FilterFIREnum.pm_C15_1, "PM C15; 395/512/532/650 Hz; -30/-5/-30 dB; 276 order; 48 kHz"},
-            {FilterFIREnum.pm_C15_2, "PM C15; 463/512/532/581 Hz; -40/-3/-40 dB; 1060 order; 48 kHz"}
+            {FilterFIREnum.pm_C15_2, "PM C15; 463/512/532/581 Hz; -40/-3/-40 dB; 1060 order; 48 kHz"},
+            {FilterFIREnum.pm_C15_3, "PM C15; 395/512/532/650 Hz; -60/-5/-60 dB; 661 order; 48 kHz"}
         };
     }
 
